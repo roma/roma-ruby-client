7ac6cd0952590d1ef3edb0f95ed25246aa5e6572
yukio.goto <yukio.goto@mail.rakuten.com>
2011-11-21 16:45:33 +0900

    add spec helper and spec option file.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+require 'rspec'
+
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+Dir[File.dirname(__FILE__) + "/supports/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.mock_with :rr
+end
+
