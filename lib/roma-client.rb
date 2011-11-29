8057f7305f3082df6cdf0540268db2df80db89a4
yukio.goto <yukio.goto@mail.rakuten.com>
2011-11-29 17:34:05 +0900

    add ClientPool class.
    add ClientPool class and ClientPool spec for singleton

--- lib/roma-client.rb
+++ lib/roma-client.rb
@@ -2,6 +2,8 @@ require File.expand_path(File.join('roma', 'client'),
                          File.dirname(__FILE__))
 require File.expand_path(File.join('roma', 'client', 'version'),
                          File.dirname(__FILE__))
+require File.expand_path(File.join('roma', 'client', 'client_pool'),
+                         File.dirname(__FILE__))
 
 if defined?(Rails)
   require File.join(File.dirname(__FILE__),
