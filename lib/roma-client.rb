d356ab5f9f50b3c4054843b25797f180a0893c05
yukio.goto <yukio.goto@mail.rakuten.com>
2011-12-19 11:56:06 +0900

    remove require RomaStore .
    I remove RomaStore from roma-client.

--- lib/roma-client.rb
+++ lib/roma-client.rb
@@ -4,9 +4,3 @@ require File.expand_path(File.join('roma', 'client', 'version'),
                          File.dirname(__FILE__))
 require File.expand_path(File.join('roma', 'client', 'client_pool'),
                          File.dirname(__FILE__))
-
-if defined?(Rails)
-  require File.join(File.dirname(__FILE__),
-                    'action_dispatch', 'middleware',
-                    'session', 'roma_store')
-end
